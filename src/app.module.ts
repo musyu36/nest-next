d156cf9b24097ac3a9248d6a483d4d6193433a86
musyu36 <musyu36@gmail.com>
2023-05-07 17:46:38 +0900

    モジュールをnest g moduleコマンドで作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 // DIを機能させるために必要な記述
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, TodoModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
